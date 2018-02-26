13e00a6e1676d58bc911efa69b11c1b2d72c16ea
xiaoqiang <756164814@qq.com>
2018-02-26 14:48:53 +0800

    A.添加AES工具类

---
+++ server/src/main/java/com/topschool/xm/util/AESUtil.java
@@ -0,0 +1,60 @@
+package com.topschool.xm.util;
+
+import org.apache.commons.codec.binary.Base64;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.*;
+import java.security.spec.InvalidParameterSpecException;
+
+
+public class AESUtil {
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    /**
+     * AES解密
+     *
+     * @param data           //密文，被加密的数据
+     * @param key            //秘钥
+     * @param iv             //偏移量
+     * @param encodingFormat //解密后的结果需要进行的编码
+     * @return
+     */
+    public static String decrypt(String data, String key, String iv, String encodingFormat) {
+
+        //被加密的数据
+        byte[] dataByte = Base64.decodeBase64(data);
+        //加密秘钥
+        byte[] keyByte = Base64.decodeBase64(key);
+        //偏移量
+        byte[] ivByte = Base64.decodeBase64(iv);
+
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
+            Key spec = new SecretKeySpec(keyByte, "AES");
+
+            AlgorithmParameters parameters = AlgorithmParameters.getInstance("AES");
+            parameters.init(new IvParameterSpec(ivByte));
+
+            cipher.init(Cipher.DECRYPT_MODE, spec, parameters);
+
+            byte[] resultByte = cipher.doFinal(dataByte);
+            if (null != resultByte && resultByte.length > 0) {
+                return new String(resultByte, encodingFormat);
+            }
+            return null;
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException | BadPaddingException | IllegalBlockSizeException | InvalidAlgorithmParameterException | InvalidKeyException | InvalidParameterSpecException | NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
