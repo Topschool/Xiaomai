f2a77a558093c751c1a7215a384bee303d8c8a22
xiaoqiang <756164814@qq.com>
2018-03-03 17:38:27 +0800

    M.添加order相关

---
+++ server/src/main/java/com/topschool/xm/dao/orderfood/OrderDao.java
@@ -0,0 +1,22 @@
+package com.topschool.xm.dao.orderfood;
+
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
+import com.topschool.xm.model.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface OrderDao {
+
+    int insert(@Param("pojo") Order pojo);
+
+    int insertList(@Param("pojos") List< Order> pojo);
+
+    List<Order> select(@Param("pojo") Order pojo);
+
+    Order selectByUid(@Param("uid")Long uid, @Param("now")Long now);
+
+    int update(@Param("pojo") Order pojo);
+
+    int deleteByOrderId(Long id);
+}
